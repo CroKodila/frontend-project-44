b8c7f7360a0324aa4a0404a0b45a855a3b09f98b
CroKodila <kostyaantonov999@gmail.com>
2023-12-14 19:40:47 +0300

    fix: duplication

--- src/games/brain-calc-logic.js
+++ src/games/brain-calc-logic.js
@@ -14,17 +14,17 @@ function brainCalc() {
     number2[i] = Math.round(Math.random() * 10);
     if (operationCode[i] === 1) {
       correctAnswers[i] = number1[i] + number2[i];
-      sign = "+"
+      sign = " + "
     } else if (operationCode[i] === 2) {
       correctAnswers[i] = number1[i] - number2[i];
-      sign = "-"
+      sign = " - "
     } else {
       correctAnswers[i] = number1[i] * number2[i];
-      sign = "*"
+      sign = " * "
     }
     gameQuestions[i] = "Question: " +
-    number1[i].toString() + " " +
-    sign + " " +
+    number1[i].toString() +
+    sign +
     number2[i].toString() +
     " ";
   }
