0850235fbdaabf0893ffbbd4a0a77b417aa1a704
CroKodila <kostyaantonov999@gmail.com>
2023-12-14 21:32:01 +0300

    fix: default export cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,7 @@
 import readlineSync from 'readline-sync';
 
-function hello() {
+export default function hello() {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
 }
-export {hello};
