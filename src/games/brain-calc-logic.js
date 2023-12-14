70963fede7222cdb5265571cc3d16f10abf70475
CroKodila <kostyaantonov999@gmail.com>
2023-12-14 19:39:42 +0300

    fix: duplicates

--- src/games/brain-calc-logic.js
+++ src/games/brain-calc-logic.js
@@ -1,3 +1,4 @@
+import { question } from "readline-sync";
 import { engine } from "../index.js";
 
 function brainCalc() {
@@ -13,29 +14,19 @@ function brainCalc() {
     number2[i] = Math.round(Math.random() * 10);
     if (operationCode[i] === 1) {
       correctAnswers[i] = number1[i] + number2[i];
-      gameQuestions[i] =
-        "Question: " +
-        number1[i].toString() +
-        " + " +
-        number2[i].toString() +
-        " ";
+      sign = "+"
     } else if (operationCode[i] === 2) {
       correctAnswers[i] = number1[i] - number2[i];
-      gameQuestions[i] =
-        "Question: " +
-        number1[i].toString() +
-        " - " +
-        number2[i].toString() +
-        " ";
+      sign = "-"
     } else {
       correctAnswers[i] = number1[i] * number2[i];
-      gameQuestions[i] =
-        "Question: " +
-        number1[i].toString() +
-        " * " +
-        number2[i].toString() +
-        " ";
+      sign = "*"
     }
+    gameQuestions[i] = "Question: " +
+    number1[i].toString() + " " +
+    sign + " " +
+    number2[i].toString() +
+    " ";
   }
   engine(task, correctAnswers, gameQuestions);
 }
